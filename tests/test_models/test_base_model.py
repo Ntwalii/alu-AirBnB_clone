1c58e68e1ec750ecf5551700e1ef2a3dfe699f44
David3D-AndweM <d.mwape@alustudent.com>
2024-01-14 20:05:35 +0200

    m

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,7 +12,7 @@ import unittest
 from datetime import datetime
 from time import sleep
 from models.base_model import BaseModel
-
+# : Review this docstring
 
 class TestBaseModel_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the BaseModel class."""
